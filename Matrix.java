5a442a224161ec1b8dd39bb7a39bdf84dc44ddcd
AravindhAmirthalingam <116623430+AravindhAmirthalingam@users.noreply.github.com>
2022-10-30 21:27:45 +0530

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,14 @@
+public class Matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a[][]= {{1,2,3},{4,5,6},{7,8,9}};
+            for (int r=0;r<3;r++) {
+            for (int s=0;s<3;s++) {
+            	System.out.print(a[s][r]+"");
+            }
+                System.out.println();
+            }
+	}
+
+}
