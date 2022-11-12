ce0ecf2d6290aa10d2955fdb33af97f463865ec8
AravindhAmirthalingam <116623430+AravindhAmirthalingam@users.noreply.github.com>
2022-11-12 12:00:16 +0530

    Create Pre Increment operator.java

---
+++ Pre Increment operator.java
@@ -0,0 +1,10 @@
+public class Incrementoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int count= 35;
+    ++count;
+    System.out.println(count);
+	}
+
+}
